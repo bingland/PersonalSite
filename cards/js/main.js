4c51c89dcacd6b9482d8b654098f2331b9aac6e3
Brayden England <bray.patriot@gmail.com>
2019-12-07 19:09:08 -0700

    Foundation; added template

--- cards/js/main.js
+++ cards/js/main.js
@@ -3,6 +3,18 @@ import { configImg } from './configImg.js'
 
 let objects = []
 let outputArea = document.querySelector(".output")
+let outputAmount = 25
+
+var addButton = document.createElement('div')
+addButton.className = 'add'
+
+//create element
+addButton.innerHTML = `
+    <div class="add">
+        <div class="addCircle">Add</div>
+    </div>
+    `
+
 
 fetch('https://ghibliapi.herokuapp.com/people', {method: 'GET'})
     .then(res => res.json())
@@ -31,12 +43,17 @@ fetch('https://ghibliapi.herokuapp.com/people', {method: 'GET'})
         });
 
         //config img
-        configImg()
+        configImg() // doesnt work yet
 
         //output to html page
-        objects.forEach(element => {
-            outputArea.appendChild(element)
-        })
+        //convert to a set amount with a for loop
+        for(let i = 0; i < outputAmount; i++){
+            outputArea.appendChild(objects[i])
+        }
+        //add the add button
+        outputArea.appendChild(addButton)
+        
+        addTheAdd()
 
         //add animation listeners
         let cards = document.getElementsByClassName("card")
@@ -45,6 +62,34 @@ fetch('https://ghibliapi.herokuapp.com/people', {method: 'GET'})
                 cards[i].classList.toggle('is-flipped')
             })
         }
+        
 
     })
     .catch(error => console.log(error))
+
+const addTheAdd = () => {
+    if (document.querySelector(".add") == 0) {
+        outputArea.appendChild(addButton)
+        console.log('hi?')
+    }
+
+    //add event listener
+    let button = document.querySelector('.add')
+    console.log(button)
+
+    button.addEventListener('click', function(){
+        
+
+        outputArea.appendChild(button)
+        // add a card
+        // get the amount of cards and update it
+        let cards = document.getElementsByClassName("card")
+        outputArea.appendChild(objects[cards.length])
+        console.log(cards.length)
+        
+        let newbutton = document.querySelector('.add')
+        newbutton.parentNode.removeChild(newbutton)
+        addTheAdd()
+    })
+
+}
